558e2840e41efb5f8865382dacee3933eb763548
BwdYeti <BwdYeti@gmail.com>
2019-03-07 13:09:47 -0500

    Weapon Triangle Arrow
    Show weapon triangle arrow/effectiveness/etc on the combat preview map info window
    Add Weapon_Triangle_Arrow.SetWeaponTriangle() to simplify refreshing weapon triangle arrow values
    Add Weapon_Triangle_Arrow.set_effectiveness() virtual method for Effective_WT_Arrow to inherit

--- FEXNA/Source Code/Sprites/Sparks/Weapon_Triangle_Arrow.cs
+++ FEXNA/Source Code/Sprites/Sparks/Weapon_Triangle_Arrow.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using FEXNAWeaponExtension;
 
 namespace FEXNA
 {
@@ -43,6 +44,75 @@ namespace FEXNA
                     return new Rectangle(0, 0, 0, 0);
                 return new Rectangle(7 * (Timer / 8), 48 + (index % 2) * 10, 7, 10);
             }
+        }
+
+        public virtual void set_effectiveness(int effectiveness, bool halved = false) { }
+
+        private void ResetTriangle()
+        {
+            set_effectiveness(1);
+            this.value = WeaponTriangle.Nothing;
+        }
+        
+        public static void ResetWeaponTriangle(
+            Weapon_Triangle_Arrow wta1,
+            Weapon_Triangle_Arrow wta2)
+        {
+            wta1.ResetTriangle();
+            if (wta2 != null)
+                wta2.ResetTriangle();
+        }
+
+        public static void SetWeaponTriangle(
+            Weapon_Triangle_Arrow wta1,
+            Weapon_Triangle_Arrow wta2,
+            Game_Unit attacker,
+            Combat_Map_Object target,
+            FEXNA_Library.Data_Weapon weapon1,
+            FEXNA_Library.Data_Weapon weapon2,
+            int distance)
+        {
+            //@Yeti: use in all the places weapon triangle arrow is used
+            Game_Unit targetUnit = null;
+            if (target.is_unit())
+                targetUnit = target as Game_Unit;
+
+            ResetWeaponTriangle(wta1, wta2);
+            WeaponTriangle tri = WeaponTriangle.Nothing;
+
+            if (targetUnit != null)
+            tri = Combat.weapon_triangle(attacker, targetUnit, weapon1, weapon2, distance);
+            if (tri != WeaponTriangle.Nothing)
+            {
+                wta1.value = tri;
+                if (wta2 != null && weapon2 != null)
+                    wta2.value = Combat.reverse_wta(tri);
+            }
+
+            float effectiveness = weapon1.effective_multiplier(attacker, targetUnit, false);
+            wta1.set_effectiveness((int)effectiveness,
+                targetUnit != null && targetUnit.halve_effectiveness());
+
+            if (wta2 != null && weapon2 != null)
+            {
+                effectiveness = weapon2.effective_multiplier(targetUnit, attacker, false);
+                wta2.set_effectiveness((int)effectiveness, attacker.halve_effectiveness());
+            }
+        }
+
+        public static void SetWeaponTriangle(
+            Weapon_Triangle_Arrow wta,
+            Game_Unit attacker,
+            Combat_Map_Object target,
+            FEXNA_Library.Data_Weapon weapon1,
+            FEXNA_Library.Data_Weapon weapon2,
+            int distance)
+        {
+            SetWeaponTriangle(
+                wta, null,
+                attacker, target,
+                weapon1, weapon2,
+                distance);
         }
     }
 }
