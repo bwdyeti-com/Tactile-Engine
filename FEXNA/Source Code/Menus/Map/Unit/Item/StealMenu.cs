2b67186543328c1326d628ad034acaf01d2f2e56
BwdYeti <BwdYeti@gmail.com>
2019-05-17 14:55:56 -0400

    UnitMenuManager
    Add UnitMenuManager, UnitCommandMenu, and other menus for units

---
+++ FEXNA/Source Code/Menus/Map/Unit/Item/StealMenu.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using FEXNA.Windows.Command.Items;
+
+namespace FEXNA.Menus.Map.Unit.Item
+{
+    class StealMenu : ItemMenu
+    {
+        public StealMenu(Window_Steal stealWindow, IHasCancelButton menu = null) : base(stealWindow, menu) { }
+        
+        protected override void SelectItem()
+        {
+            var stealWindow = (this.Window as Window_Steal);
+
+            if (stealWindow.is_equipped(stealWindow.redirect()))
+                stealWindow.equipped_steal_help();
+            else if (!stealWindow.can_steal(stealWindow.redirect()))
+                stealWindow.cant_steal_help();
+            else
+            {
+                OnSelected(new EventArgs());
+            }
+        }
+    }
+}
---
+++ FEXNA/Source Code/Menus/Map/Unit/Target/UnitTargetMenu.cs
@@ -0,0 +1,17 @@
+﻿using FEXNA.Windows.Target;
+
+namespace FEXNA.Menus.Map.Unit.Target
+{
+    class UnitTargetMenu : TargetMenu<int>
+    {
+        public UnitTargetMenu(Window_Target_Unit window, IHasCancelButton menu = null)
+            : base(window, menu) { }
+
+        public int SelectedUnitId
+        {
+            get { return Window.targets[Window.selected_index()]; }
+        }
+
+        public int Target { get { return Window.target; } }
+    }
+}
