78bd5b04a46c96d7479e89d928d4ab3d0a3225cd
BwdYeti <BwdYeti@gmail.com>
2019-08-05 14:59:05 -0400

    UI
    Add StatusWindowDivider, remove Shop_Window_Divider
    Use StatusWindowDivider as a parent class for Status_Bonus_Background, Status_Support_Background
    Use a constant for previewing UI connections in DEBUG

--- FEXNA/Source Code/Sprites/Windowskins/StatusWindowDivider.cs
+++ FEXNA/Source Code/Sprites/Windowskins/StatusWindowDivider.cs
@@ -3,18 +3,18 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace FEXNA
 {
-    class Shop_Window_Divider : Sprite
+    class StatusWindowDivider : Sprite
     {
         private int Width;
 
-        #region Accessors
-        public int width { set { Width = (value / 8) * 8; } }
-        #endregion
-
-        public Shop_Window_Divider()
+        public StatusWindowDivider()
         {
             texture = Global.Content.Load<Texture2D>(@"Graphics/Windowskins/Shop_Portrait_bg");
-            src_rect = new Rectangle(0, 64 + Global.game_options.window_color * 8, 24, 8);
+        }
+
+        public void SetWidth(int value)
+        {
+            Width = (value / 8) * 8;
         }
 
         public override void draw(SpriteBatch sprite_batch, Vector2 draw_offset = default(Vector2))
@@ -22,19 +22,20 @@ namespace FEXNA
             if (texture != null)
                 if (visible && Width >= 16)
                 {
+                    int srcY = 64 + Global.game_options.window_color * 8;
                     int x = 0;
                     // Left
                     sprite_batch.Draw(texture, (loc + new Vector2(x, 0) + draw_vector()) - draw_offset,
-                        new Rectangle(0, 64 + Global.game_options.window_color * 8, 8, 8), tint, angle, offset, scale,
+                        new Rectangle(0, srcY, 8, 8), tint, angle, offset, scale,
                         mirrored ? SpriteEffects.FlipHorizontally : SpriteEffects.None, Z);
                     // Middle
-                    for (x += 8; x < Width - 8; x+= 8 )
+                    for (x += 8; x < Width - 8; x += 8)
                         sprite_batch.Draw(texture, (loc + new Vector2(x, 0) + draw_vector()) - draw_offset,
-                            new Rectangle(8, 64 + Global.game_options.window_color * 8, 8, 8), tint, angle, offset, scale,
+                            new Rectangle(8, srcY, 8, 8), tint, angle, offset, scale,
                             mirrored ? SpriteEffects.FlipHorizontally : SpriteEffects.None, Z);
                     // Right
                     sprite_batch.Draw(texture, (loc + new Vector2(x, 0) + draw_vector()) - draw_offset,
-                        new Rectangle(16, 64 + Global.game_options.window_color * 8, 8, 8), tint, angle, offset, scale,
+                        new Rectangle(16, srcY, 8, 8), tint, angle, offset, scale,
                         mirrored ? SpriteEffects.FlipHorizontally : SpriteEffects.None, Z);
                 }
         }
