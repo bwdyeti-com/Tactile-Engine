9c4b50640581cb1540c5ce3d6729996ac6b402b6
BwdYeti <BwdYeti@gmail.com>
2022-05-22 20:38:54 -0400

    Unit Screen Pointing Controls
    -Convert Window_Unit into using standard UINode functionality and mouse/touch support
    -Add UnitScreenHeader to make a collection of the active unit screen keys

---
+++ Tactile/Source Code/Windows/Map Menus/UnitScreenHeader.cs
@@ -0,0 +1,19 @@
+﻿namespace Tactile
+{
+    struct UnitScreenHeader
+    {
+        public int Index { get; private set; }
+        public string Name { get; private set; }
+        public int Page { get; private set; }
+
+        public UnitScreenHeader(
+            int index,
+            string name,
+            int page)
+        {
+            Index = index;
+            Name = name;
+            Page = page;
+        }
+    }
+}
